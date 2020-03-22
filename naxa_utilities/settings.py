680d25208b50993826e64e44f2677fd1c0f4d942
sumit <rounnn8@gmail.com>
2020-03-22 22:46:43 +0545

    cors

--- naxa_utilities/settings.py
+++ naxa_utilities/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'naxa_utilities.urls'
