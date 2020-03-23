77407605f9a1e620f7b8e3c1107fac69593bc1ab
sumit <rounnn8@gmail.com>
2020-03-23 12:37:59 +0545

    setting gunicorn for production

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -2,5 +2,4 @@
 python manage.py collectstatic --no-input
 # python manage.py compilemessages -l ne -l en
 python manage.py migrate --no-input
-pytest
-python manage.py runserver 0.0.0.0:8000
+gunicorn naxa_utilities.wsgi:application --bind 0.0.0.0:8000
