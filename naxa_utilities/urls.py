59dba574a7d46b409e42d09ba61c72b33dc49407
sumit <rounnn8@gmail.com>
2020-03-23 18:32:21 +0545

    default view swagger

--- naxa_utilities/urls.py
+++ naxa_utilities/urls.py
@@ -30,7 +30,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include('api.urls')),
 
 ]
