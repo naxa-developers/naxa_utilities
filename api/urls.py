cb766bb75992d13c87872ba5a6b2c87a257e8c06
sumit <rounnn8@gmail.com>
2020-03-23 14:50:27 +0545

    all data post /update/delete api created

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
 from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'province', views.ProvinceApi)
+router.register(r'province-data', views.ProvinceDataApi)
+router.register(r'cases', views.CaseApi)
+router.register(r'health-type', views.MedicalTypeApi)
+router.register(r'health-category', views.MedicalCategoryApi)
+router.register(r'health-facility', views.MedicalApi)
 
 urlpatterns = [
-    path('health-facility/', views.MedicalApi.as_view({'get': 'list'}), name='health-facility'),
-    path('health-category/', views.MedicalCategoryApi.as_view({'get': 'list'}), name='health-category'),
-    path('health-type/', views.MedicalTypeApi.as_view({'get': 'list'}), name='health-type'),
-    path('cases/', views.CaseApi.as_view({'get': 'list'}), name='cases'),
-    path('province/', views.ProvinceApi.as_view({'get': 'list'}), name='province'),
-    path('province-data/', views.ProvinceDataApi.as_view({'get': 'list'}), name='province-data'),
+    path('', include(router.urls)),
+    # path('health-facility/', views.MedicalApi.as_view({'get': 'list'}), name='health-facility'),
+    # path('health-category/', views.MedicalCategoryApi.as_view({'get': 'list'}), name='health-category'),
+    # path('health-type/', views.MedicalTypeApi.as_view({'get': 'list'}), name='health-type'),
+    # path('cases/', views.CaseApi.as_view({'get': 'list'}), name='cases'),
+    # path('province-data/', views.ProvinceDataApi.as_view({'get': 'list'}), name='province-data'),
 ]
--- api/views.py
+++ api/views.py
@@ -9,88 +9,96 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
-class MedicalApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
+class MedicalApi(viewsets.ModelViewSet):
+    queryset = MedicalFacility.objects.order_by('id')
+    serializer_class = MedicalFacilitySerializer
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'type']
 
-    def get_queryset(self):
-        queryset = MedicalFacility.objects.order_by('id')
-        return queryset
-
-    def get_serializer_class(self):
-        serializer_class = MedicalFacilitySerializer
-        return serializer_class
-
-
-class MedicalCategoryApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
-
-    def get_queryset(self):
-        queryset = MedicalFacilityCategory.objects.order_by('id')
-        return queryset
-
-    def get_serializer_class(self):
-        serializer_class = MedicalFacilityCategorySerializer
-        return serializer_class
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
 
-class MedicalTypeApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
+class MedicalCategoryApi(viewsets.ModelViewSet):
+    queryset = MedicalFacilityCategory.objects.order_by('id')
+    serializer_class = MedicalFacilityCategorySerializer
 
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+
+class MedicalTypeApi(viewsets.ModelViewSet):
+    queryset = MedicalFacilityType.objects.order_by('id')
+    serializer_class = MedicalFacilityTypeSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'category']
 
-    def get_queryset(self):
-        queryset = MedicalFacilityType.objects.order_by('id')
-        return queryset
-
-    def get_serializer_class(self):
-        serializer_class = MedicalFacilityTypeSerializer
-        return serializer_class
-
-
-class CaseApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
-
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['id', ]
-
-    def get_queryset(self):
-        queryset = CovidCases.objects.order_by('id')
-        return queryset
-
-    def get_serializer_class(self):
-        serializer_class = CaseSerializer
-        return serializer_class
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
 
-class ProvinceApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
+class CaseApi(viewsets.ModelViewSet):
+    queryset = CovidCases.objects.order_by('id')
+    serializer_class = CaseSerializer
 
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['id', ]
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
-    def get_queryset(self):
-        queryset = Province.objects.order_by('id')
-        return queryset
 
-    def get_serializer_class(self):
-        serializer_class = ProvinceSerializer
-        return serializer_class
+class ProvinceApi(viewsets.ModelViewSet):
+    queryset = Province.objects.order_by('id')
+    serializer_class = ProvinceSerializer
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
 
 
-class ProvinceDataApi(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [AllowAny]
+class ProvinceDataApi(viewsets.ModelViewSet):
+    queryset = ProvinceData.objects.order_by('id')
+    serializer_class = ProvinceDataSerializer
 
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['id', ]
-
-    def get_queryset(self):
-        queryset = ProvinceData.objects.order_by('id')
-        return queryset
-
-    def get_serializer_class(self):
-        serializer_class = ProvinceDataSerializer
-        return serializer_class
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create' or self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
