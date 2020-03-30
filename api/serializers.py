5b12e7a93cd70f7179254d07c8cb0dd167e93f60
awemulya <awemulya@gmail.com>
2020-03-30 12:18:16 +0545

    create user

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 import random
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import MedicalFacility, MedicalFacilityCategory, \
     MedicalFacilityType, CovidCases, Province, ProvinceData, District, \
@@ -211,4 +212,11 @@ class MobileVersionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MobileVersion
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
         fields = "__all__"
