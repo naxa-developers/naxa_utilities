063e9e27208975ca827a53d9f9e223b856dd3f4a
awemulya <awemulya@gmail.com>
2020-04-09 18:00:57 +0545

    settings debug mode

--- naxa_utilities/urls.py
+++ naxa_utilities/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
+if settings.DEBUG or True:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
